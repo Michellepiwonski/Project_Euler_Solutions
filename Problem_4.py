1dc293215bad5585d50e30e158e0168d770513dd
Michellepiwonski <101600328+Michellepiwonski@users.noreply.github.com>
2022-03-15 17:33:15 -0400

    Update Problem_4.py
    This code returns a palindrome, but does not return the largest palindrome.

--- Problem_4.py
+++ Problem_4.py
@@ -2,10 +2,17 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+def reverser(n):
+    return n [::-1]
+
 def palindrome():
     a = 999
     b = 999
-    while a <= 1:
-        product_hold = []
-        product_hold.append(a * b)
-        
+    while b >= 1:
+        while a >= 1:
+            if str(a * b) == reverser(str(a * b)):
+                return str(a * b)
+            a -= 1
+        b -= 1
+
+print(palindrome())
